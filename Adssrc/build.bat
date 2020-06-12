f7585520804e71e493df6ef13f6c74bcc761d244
Jon <jon@Jons-MBP.hitronhub.home>
2020-06-12 12:30:46 -0700

    ADS Update build.

--- Adssrc/build.bat
+++ Adssrc/build.bat
@@ -1,3 +1,6 @@
-:: C:\Users\jon\Dropbox\ArmaAutomotive\Adssrc\ArmaDesignStudio 
-
-"C:\Users\jon\Programs\apache-ant-1.10.8\bin\ant" -buildfile ArmaDesignStudio.xml
+:: 
+:: Run the ADS ant build process.
+:: > build.bat - will compile and build the project jar file.
+:: > build.bat exe - will compile and generate a windows exe binary.
+::
+"C:\Users\jon\Programs\apache-ant-1.10.8\bin\ant" -buildfile ArmaDesignStudio.xml %1
