3b91364b7e5d5238002ba75d7298ec78e7df336b
Jon <jon@Jons-MBP.hitronhub.home>
2021-04-14 08:46:51 -0700

    ADS adding GCode import option. Preliminary only.

--- Adssrc/ArmaDesignStudio/src/armadesignstudio/LayoutWindow.java
+++ Adssrc/ArmaDesignStudio/src/armadesignstudio/LayoutWindow.java
@@ -1,5 +1,5 @@
 /* Copyright (C) 1999-2013 by Peter Eastman
-                 2020 by Jon Taylor
+                 2020-2021 by Jon Taylor
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
